8affff222f93b51a98e7011243a7bf45c268447e
matthew1796 <35312259+matthew1796@users.noreply.github.com>
2020-11-11 17:23:05 -0800

    Add files via upload
    Base XCode solution to build off of.

---
+++ Draw and Play Video Game/Draw and Play Video GameTests/Draw_and_Play_Video_GameTests.swift
@@ -0,0 +1,33 @@
+//
+//  Draw_and_Play_Video_GameTests.swift
+//  Draw and Play Video GameTests
+//
+//  Created by user183570 on 11/11/20.
+//
+
+import XCTest
+@testable import Draw_and_Play_Video_Game
+
+class Draw_and_Play_Video_GameTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
