4a58ec224317991236bd999a53c7d95884e09145
Ying <liyi4@oregonstate.edu>
2021-02-16 02:24:26 -0800

    attached to opencv

--- DAPGAME/DAPGAME/ViewController.swift
+++ DAPGAME/DAPGAME/ViewController.swift
@@ -7,27 +7,39 @@
 
 import UIKit
 
-var imagePicker: UIImagePickerController!
-
-
-
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBAction func takePhoto(_ sender: Any) {
-            imagePicker =  UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = .camera
-
-            present(imagePicker, animated: true, completion: nil)
-    }
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            imagePicker.dismiss(animated: true, completion: nil)
-            imageView.image = info[.originalImage] as? UIImage
+        let actionSheetController = UIAlertController(title: "Choose your image", message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel) { (alertAction) -> Void in
+            //do nothing
         }
+        
+        let takingPicturesAction = UIAlertAction(title: "Take Photo", style: UIAlertAction.Style.destructive) { (alertAction) -> Void in
+            self.getImageGo(type: 1)
+        }
+        
+        let photoAlbumAction = UIAlertAction(title: "Album", style: UIAlertAction.Style.default) { (alertAction) -> Void in
+            self.getImageGo(type: 2)
+        }
+                
+        actionSheetController.addAction(cancelAction)
+        actionSheetController.addAction(takingPicturesAction)
+        actionSheetController.addAction(photoAlbumAction)
+        
+        //Set anchor point for iPad device floating layer
+        actionSheetController.popoverPresentationController?.sourceView = sender as? UIView
+        //show
+        self.present(actionSheetController, animated: true, completion: nil)
+    }
+    
     @IBOutlet weak var imageView: UIImageView!
+    var takingPicture:UIImagePickerController!
     
     override func viewDidLoad() {	
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
 //        let button = UIButton.init(type: .custom)
 //
 //                view.addSubview(button)
@@ -35,7 +47,36 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
 //                button.frame = CGRect.init(x: 100, y: 100, width: 200, height: 30)
 //                button.tag = 1
 //                button.addTarget(self, action: #selector(tapped), for: .touchUpInside)
-
+    }
+    
+    
+    func getImageGo(type:Int){
+        takingPicture =  UIImagePickerController.init()
+        if(type==1){
+            takingPicture.sourceType = .camera
+            //Whether to show the toolbar when taking pictures
+            //takingPicture.showsCameraControls = true
+        }else if(type==2){
+            takingPicture.sourceType = .photoLibrary
+        }
+        //Whether to intercept, set to true to intercept the picture into a square after obtaining it
+        takingPicture.allowsEditing = false
+        takingPicture.delegate = self
+        present(takingPicture, animated: true, completion: nil)
+    }
+    
+    //Take a photo or choose the returned picture from the album
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        takingPicture.dismiss(animated: true, completion: nil)
+        if(takingPicture.allowsEditing == false){
+            //Original image
+            let resultImage = LineDetectorBridge().detectLine(in: info[UIImagePickerController.InfoKey.originalImage]as? UIImage)
+            imageView.image = resultImage
+        }else{
+            //Screenshot; Unprocessed
+            imageView.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        }
+ 
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
